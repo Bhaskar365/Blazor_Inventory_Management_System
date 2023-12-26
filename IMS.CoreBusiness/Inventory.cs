5a31e724809ec2f4c051487f8600165d68f2e779
bdas615 <bdas@brandinstitute.com>
2023-12-26 21:22:54 +0530

    new class libraries added and repositories added to test seeding dataa

---
+++ IMS.CoreBusiness/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
+{
+    public class Inventory
+    {
+        public int InventoryId { get; set; }
+        [Required]
+        public string? InventoryName { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
