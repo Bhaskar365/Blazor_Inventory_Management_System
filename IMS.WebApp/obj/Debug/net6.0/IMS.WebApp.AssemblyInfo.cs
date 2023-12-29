1ececdc89a1e4c06cbadbb0b0b3a5136decaa89c
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2023-12-29 22:37:04 +0530

    counter razor page code changes

--- IMS.WebApp/obj/Debug/net6.0/IMS.WebApp.AssemblyInfo.cs
+++ IMS.WebApp/obj/Debug/net6.0/IMS.WebApp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IMS.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d58c67bcd6604bcd9ee202bc623199884dcb8644")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+352c3005bc994e23c8c3234526e9607441320b87")]
 [assembly: System.Reflection.AssemblyProductAttribute("IMS.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IMS.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
